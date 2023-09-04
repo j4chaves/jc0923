49d51a1a0e56d98a8f62ed0e1853068524ebf955
Jacob Chaves <jacobjc3@gmail.com>
2023-09-04 15:46:53 -0400

    initial commit

---
+++ src/main/java/com/jc0923/toolrental/ToolRentalMain.java
@@ -0,0 +1,13 @@
+package com.jc0923.toolrental;
+
+/**
+ * Hello world!
+ *
+ */
+public class ToolRentalMain 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "JC Tool Rental" );
+    }
+}
