44cc925ff77d509322e4d30f49cfe5f2f9ee6dae
Jacob Chaves <jacobjc3@gmail.com>
2023-09-08 16:11:47 -0400

    Labor day logic, needs further testing

--- src/main/java/com/jc0923/toolrental/services/HolidayService.java
+++ src/main/java/com/jc0923/toolrental/services/HolidayService.java
@@ -1,10 +1,11 @@
 package com.jc0923.toolrental.services;
 
+import java.time.DayOfWeek;
 import java.time.LocalDate;
+import java.time.temporal.TemporalAdjuster;
+import java.time.temporal.TemporalAdjusters;
 
 public class HolidayService {
-	//TODO Write unit tests for this service
-	//Need to take into account the day the holiday is observed for other logic, not necessarily in this file
 
 	public static boolean isIndependenceDayInDateRange(LocalDate startDate, LocalDate endDate) {
 		boolean inDateRange = false;
@@ -31,8 +32,21 @@ public class HolidayService {
 		return inDateRange;
 	}
 	
-//	public static boolean isLaborDayInDateRange(LocalDate startDate, LocalDate endDate) {
-//		LocalDate laborDay = LocalDate.of 
-//				DayOfWeek.
-//	}
+	public static boolean isLaborDayInDateRange(LocalDate startDate, LocalDate endDate) {
+		boolean inDateRange = false;
+
+		if (startDate.getMonth().getValue() <= 9 && endDate.getMonth().getValue() >= 9) {
+			LocalDate laborDay = LocalDate.of(startDate.getYear(), 9, 1);
+			TemporalAdjuster laborDayAdjuster = TemporalAdjusters.firstInMonth(DayOfWeek.MONDAY);
+			laborDay.with(laborDayAdjuster);	//Set variable to first Monday of September 
+			
+			if (laborDay.isAfter(startDate) && laborDay.isBefore(endDate)) {
+				inDateRange = true;
+			}
+		} else {
+			inDateRange = false;
+		}
+		
+		return inDateRange;
+	}
 }
